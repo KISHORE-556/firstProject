5715601159f2ca21f0cd070b5a2d95c3f05bfc10
Kishore <kisho@Warrior_556>
2024-05-31 17:48:53 +0530

    intial commit

---
+++ src/main/java/dev/kishore/firstproject/Controller/firstProject.java
@@ -0,0 +1,16 @@
+package dev.kishore.firstproject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class firstProject {
+
+    @GetMapping("/hello")
+    public String sayHello()
+    {
+        return "Hello World!!";
+    }
+
+
+}
